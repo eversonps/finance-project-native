d152b1680cdebfc06828d245491473cfba7ba174
Everson <eversonpereiraofc@gmail.com>
2022-07-20 16:23:49 -0300

    register, login and logout user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import { AuthContext } from "../contexts/auth";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
+import { View, ActivityIndicator } from "react-native";
+
 export default function Routes(){
+    const { signed, loading } = useContext(AuthContext)
+
+    if(loading){
+        return (
+            <View styles={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                <ActivityIndicator size="large" color="#131313"/>
+            </View>
+        )
+    }
+
     return (
-        <AuthRoutes />
+        signed ? <AppRoutes /> : <AuthRoutes />
     )
 }
