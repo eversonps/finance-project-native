3f68b52d864c1389ff0577a4292e869f437e1062
Everson <eversonpereiraofc@gmail.com>
2022-07-21 11:47:42 -0300

    create home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
-export default function Home(){
-    const {signOut} = useContext(AuthContext)
+import Header from "../../components/Header";
+import { Background, Container, Saldo, Title, Nome, List} from "./styles"
+import HistoricoList from "../../components/HistoricoList";
 
+export default function Home(){
+    const {user, signOut} = useContext(AuthContext)
+
+    const [historico, setHistorico] = useState([
+        {key: '1', tipo: 'receita', valor: 1200},
+        {key: '2', tipo: 'despesa', valor: 200},
+        {key: '3', tipo: 'despesa', valor: 40},
+        {key: '4', tipo: 'receita', valor: 89.62},
+    ])
     return (
-        <View>
-            <Text>Home</Text>
-            <Button 
-            title="Sair da conta"
-            onPress={() => signOut()}
+        <Background>
+            <Header />
+            <Container>
+                <Nome>Everson</Nome>
+                <Saldo>R$ 100,99</Saldo>
+            </Container>
+
+            <Title>Ultimas movimentações</Title>
+
+            <List 
+            showsVerticalScrollIndicator={false}
+            data={historico}
+            keyExtractor={item => item.key}
+            renderItem={({item}) => (<HistoricoList data={item} />)}
             />
-        </View>
+        </Background>
     )
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,12 +5,15 @@ import { useNavigation } from "@react-navigation/native";
 
 import { Container, Nome, NewLink, NewText, Logout, LogoutText } from "./styles";
 
+import Header from "../../components/Header";
+
 export default function Profile(){
     const {signOut, user} = useContext(AuthContext)
     const navigation = useNavigation(AuthContext)
     
     return (
         <Container>
+            <Header />
             <Nome>{user && user.nome}</Nome>
             <NewLink onPress={() => navigation.navigate("Registrar")}>
                 <NewText>Registrar gastos</NewText>
