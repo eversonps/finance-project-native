86d7c14685c9ad3656aa15de87a3b648be394cec
Everson <eversonpereiraofc@gmail.com>
2022-07-21 10:19:49 -0300

    create routes and doing profile screen

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { View, Text, Button } from "react-native";
+import { AuthContext } from "../../contexts/auth";
+import { useNavigation } from "@react-navigation/native";
+
+import { Container, Nome, NewLink, NewText, Logout, LogoutText } from "./styles";
+
+export default function Profile(){
+    const {signOut, user} = useContext(AuthContext)
+    const navigation = useNavigation(AuthContext)
+    
+    return (
+        <Container>
+            <Nome>{user && user.nome}</Nome>
+            <NewLink onPress={() => navigation.navigate("Registrar")}>
+                <NewText>Registrar gastos</NewText>
+            </NewLink>
+
+            <Logout onPress={() => signOut()}>
+                <LogoutText>Sair</LogoutText>
+            </Logout>
+        </Container>
+    )
+}
